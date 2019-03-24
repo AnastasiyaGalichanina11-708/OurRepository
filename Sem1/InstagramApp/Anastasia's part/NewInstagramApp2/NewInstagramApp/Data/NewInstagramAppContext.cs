34ba4902632072581cadc0c52e5eace8a7cfbbd2
Galichanina_Anastasiya_11-708 <31696192+AnastasiyaGalichanina11-708@users.noreply.github.com>
2019-03-24 20:59:17 +0300

    Add files via upload

---
+++ Sem1/InstagramApp/Anastasia's part/NewInstagramApp2/NewInstagramApp/Data/NewInstagramAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace NewInstagramApp.Models
+{
+    public class NewInstagramAppContext : DbContext
+    {
+        public NewInstagramAppContext (DbContextOptions<NewInstagramAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> User { get; set; }
+        
+    }
+}
---
+++ Sem1/InstagramApp/Anastasia's part/NewInstagramApp2/NewInstagramApp/Models/Like.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InstagramFramework.Models
+{
+    public class Like
+    {
+        public int LikeID { get; set; }
+        public string PostID { get; set; }
+    }
+}
