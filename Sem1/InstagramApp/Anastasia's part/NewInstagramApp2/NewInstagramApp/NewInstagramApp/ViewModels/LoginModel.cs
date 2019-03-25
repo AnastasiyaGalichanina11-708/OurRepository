558feaa24575632251cb0e31d7d78c61e21ade78
Galichanina_Anastasiya_11-708 <31696192+AnastasiyaGalichanina11-708@users.noreply.github.com>
2019-03-25 22:59:45 +0300

    Add files via upload

---
+++ Sem1/InstagramApp/Anastasia's part/NewInstagramApp2/NewInstagramApp/NewInstagramApp/ViewModels/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewInstagramApp.ViewModels
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Не указан Email")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Не указан пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
